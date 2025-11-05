172637822feb8bb36a3e949d9b7c9e88c8ce5a1e
PurpleMyst <PurpleMyst@users.noreply.github.com>
2025-11-06 00:30:00 +0100

    perf(quest03): better parse

--- quest03/src/lib.rs
+++ quest03/src/lib.rs
@@ -1,5 +1,7 @@
 use std::fmt::Display;
 
+use atoi::atoi;
+
 #[inline]
 pub fn solve() -> (impl Display, impl Display, impl Display) {
     (solve_part1(), solve_part2(), solve_part3())
@@ -9,8 +11,9 @@ pub fn solve() -> (impl Display, impl Display, impl Display) {
 pub fn solve_part1() -> impl Display {
     let mut crates = include_str!("part1.txt")
         .trim()
-        .split(',')
-        .map(|s| s.parse().unwrap())
+        .as_bytes()
+        .split(|&c| c == b',')
+        .map(|s| atoi(s).unwrap())
         .collect::<Vec<u8>>();
     crates.sort_unstable();
     crates.dedup();
@@ -21,8 +24,9 @@ pub fn solve_part1() -> impl Display {
 pub fn solve_part2() -> impl Display {
     let mut crates = include_str!("part2.txt")
         .trim()
-        .split(',')
-        .map(|s| s.parse().unwrap())
+        .as_bytes()
+        .split(|&c| c == b',')
+        .map(|s| atoi(s).unwrap())
         .collect::<Vec<u8>>();
     crates.sort_unstable();
     crates.dedup();
@@ -33,10 +37,10 @@ pub fn solve_part2() -> impl Display {
 pub fn solve_part3() -> impl Display {
     let crates = include_str!("part3.txt")
         .trim()
-        .split(',')
-        .map(|s| s.parse().unwrap())
+        .as_bytes()
+        .split(|&c| c == b',')
+        .map(|s| atoi(s).unwrap())
         .collect::<Vec<u8>>();
-
     let mut frequency = [0u16; 100];
     for c in crates {
         frequency[c as usize] += 1;
